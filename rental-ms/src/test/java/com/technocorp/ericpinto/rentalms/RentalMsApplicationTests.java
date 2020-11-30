cc46d9cbe75c3239b719cd28f3836ee0614b0d38
Éric Pinto <ericgrandopinto@gmail.com>
2020-11-30 16:07:04 -0300

    added inital configurations

---
+++ rental-ms/src/test/java/com/technocorp/ericpinto/rentalms/RentalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technocorp.ericpinto.rentalms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
