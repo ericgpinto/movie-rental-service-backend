972bc94c3e0f1df22e148ae38cc53f8a1a918e9d
Éric Pinto <ericgrandopinto@gmail.com>
2020-12-01 10:27:30 -0300

    added client module

---
+++ rental-ms/rental-ms-client/src/main/java/com/technocorp/ericpinto/rentalms/model/FilmResponse.java
@@ -0,0 +1,13 @@
+package com.technocorp.ericpinto.rentalms.model;
+
+import lombok.Data;
+
+@Data
+public class FilmResponse {
+
+    private String title;
+    private String episode_id;
+    private String director;
+    private String url;
+
+}
