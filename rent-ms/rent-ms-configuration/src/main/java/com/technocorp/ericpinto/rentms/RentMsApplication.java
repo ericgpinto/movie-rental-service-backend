242fe5f4f9ab59bceb31767588b026212a49984a
Éric Pinto <ericgrandopinto@gmail.com>
2020-12-01 14:48:00 -0300

    added initial configurations

---
+++ rent-ms/rent-ms-configuration/src/main/java/com/technocorp/ericpinto/rentms/RentMsApplication.java
@@ -0,0 +1,13 @@
+package com.technocorp.ericpinto.rentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentMsApplication.class, args);
+    }
+
+}
