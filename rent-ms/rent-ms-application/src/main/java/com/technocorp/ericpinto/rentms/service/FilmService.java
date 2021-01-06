33062dfddc13c0e7bb4db0e9c0545688e5517f50
Éric Pinto <ericgrandopinto@gmail.com>
2021-01-06 14:48:47 -0300

    added new changes

--- rent-ms/rent-ms-application/src/main/java/com/technocorp/ericpinto/rentms/service/FilmService.java
+++ rent-ms/rent-ms-application/src/main/java/com/technocorp/ericpinto/rentms/service/FilmService.java
@@ -2,10 +2,6 @@ package com.technocorp.ericpinto.rentms.service;
 
 import com.technocorp.ericpinto.rentms.model.Film;
 import com.technocorp.ericpinto.rentms.model.FilmResponse;
-import com.technocorp.ericpinto.rentms.model.User;
-import com.technocorp.ericpinto.rentms.repository.FilmRepostory;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- rent-ms/rent-ms-persistence/src/main/java/com/technocorp/ericpinto/rentms/model/Film.java
+++ rent-ms/rent-ms-persistence/src/main/java/com/technocorp/ericpinto/rentms/model/Film.java
@@ -1,11 +1,13 @@
 package com.technocorp.ericpinto.rentms.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Film {
 
     private String title;
-    private Integer episode_id;
+    @JsonProperty("episode_id")
+    private Integer episodeId;
     private String director;
 }
