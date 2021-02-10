89779b8453c9b17fb2ec7ad39610de5af66a7920
Éric Pinto <ericgrandopinto@gmail.com>
2021-02-10 16:47:33 -0300

    finishing unit service layer tests

--- rent-ms/rent-ms-application/src/test/java/com/technocorp/ericpinto/rentms/service/UserServiceTests.java
+++ rent-ms/rent-ms-application/src/test/java/com/technocorp/ericpinto/rentms/service/UserServiceTests.java
@@ -2,9 +2,11 @@ package com.technocorp.ericpinto.rentms.service;
 
 import com.technocorp.ericpinto.rentms.model.User;
 import com.technocorp.ericpinto.rentms.repository.UserRepository;
+import com.technocorp.ericpinto.rentms.service.exceptions.ObjectNotFoundException;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -15,8 +17,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTests {
@@ -40,7 +43,13 @@ public class UserServiceTests {
     }
 
     @Test
-    public void whenFindAll_thenReturnProductList() {
+    public void whenCreate_thenReturnUser(){
+      //  when(userRepository.save(user)).thenReturn(user);
+        assertThat(userService.create(user)).isEqualTo(user);
+    }
+
+    @Test
+    public void whenFindAll_thenReturnUserList() {
 
         List<User> expectedUsers = new ArrayList<>();
         expectedUsers.add(this.user);
@@ -63,9 +72,40 @@ public class UserServiceTests {
 
         when(userRepository.findById("5fc7ba0ee7e48d20dc2fbf52")).thenReturn(Optional.ofNullable(user));
 
-        User obj = userService.findById("5fc7ba0ee7e48d20dc2fbf52");
+        assertThat(userService.findById("5fc7ba0ee7e48d20dc2fbf52")).isEqualTo(user);
+    }
 
-        assertEquals("Éric Pinto", obj.getName());
-        assertEquals("ericgrandopinto@gmail.com", obj.getEmail());
+    @Test
+    public void shouldDelete(){
+
+        User user = User.builder()
+                .id("5fc7ba0ee7e48d20dc2fbf52")
+                .name("Éric Pinto")
+                .email("ericgrandopinto@gmail.com")
+                .build();
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        userService.delete(user.getId());
+        verify(userRepository, times(1)).deleteById(user.getId());
+
+    }
+
+    @Test
+    public void whenUpdate_thenReturnUser(){
+
+        User user = User.builder()
+                .id("5fc7ba0ee7e48d20dc2fbf52")
+                .name("Éric Pinto")
+                .email("ericgrandopinto@gmail.com")
+                .build();
+
+        when(userRepository.findById("5fc7ba0ee7e48d20dc2fbf52")).thenReturn(Optional.ofNullable(user));
+
+        user.setName("Régis");
+        user.setEmail("regis@gmail.com");
+        when(userRepository.save(user)).thenReturn(user);
+
+        assertThat(userService.update(user)).isEqualTo(user);
+
     }
 }
