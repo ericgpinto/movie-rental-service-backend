2b4b31d6b00f695156443c799c13264d8ae6b911
Éric Pinto <ericgrandopinto@gmail.com>
2021-02-23 15:11:20 -0300

    some tests were added

--- rent-ms/rent-ms-persistence/src/main/java/com/technocorp/ericpinto/rentms/model/Rent.java
+++ rent-ms/rent-ms-persistence/src/main/java/com/technocorp/ericpinto/rentms/model/Rent.java
@@ -2,18 +2,20 @@ package com.technocorp.ericpinto.rentms.model;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Builder
 @Document(collection = "rents")
 public class Rent {
 
@@ -26,8 +28,8 @@ public class Rent {
     @ApiModelProperty(value = "rented movie")
     private Film film;
     @ApiModelProperty(value = "rent start date")
-    private Date initialDate;
+    private LocalDateTime initialDate;
     @ApiModelProperty(value = "rent end date")
-    private Date finalDate;
+    private LocalDateTime finalDate;
 
 }
