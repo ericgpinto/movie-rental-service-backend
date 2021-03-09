b2fc7987ed9bf174ffc3f0d56e56fc0cd19efb39
Éric Pinto <ericgrandopinto@gmail.com>
2021-03-09 17:56:47 -0300

    resolving sonarqube coverage

--- rent-ms/rent-ms-web/src/main/java/com/technocorp/ericpinto/rentms/controller/util/URL.java
+++ rent-ms/rent-ms-web/src/main/java/com/technocorp/ericpinto/rentms/controller/util/URL.java
@@ -1,8 +1,10 @@
 package com.technocorp.ericpinto.rentms.controller.util;
 
+import lombok.NoArgsConstructor;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-
+@NoArgsConstructor
 public class URL {
 
     public static String decodeParam(String text){
